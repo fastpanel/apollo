e7d820d8a9d5696841e25a4e012d0df37ba6a4c9
Fenixphp <fenixphp@gmail.com>
2019-01-08 02:18:07 +0300

    Update core modules.

---
+++ README.md
@@ -0,0 +1,12 @@
+# fastPanel Apollo.
+Extension to work with the "Apollo" server.
+
+---
+
+## License
+The MIT License (MIT)
+
+---
+
+## Copyright
+(c) 2014 - 2019 Desionlab
---
+++ build/Extension.js
@@ -0,0 +1,29 @@
+"use strict";
+/**
+ * Extension.ts
+ *
+ * @author    Desionlab <fenixphp@gmail.com>
+ * @copyright 2014 - 2019 Desionlab
+ * @license   MIT
+ */
+Object.defineProperty(exports, "__esModule", { value: true });
+const core_1 = require("@fastpanel/core");
+/**
+ * Class Extension
+ *
+ * Initialization of the extension.
+ *
+ * @version 1.0.0
+ */
+class Extension extends core_1.Extensions.ExtensionDefines {
+    /**
+     * Registers a service provider.
+     */
+    async register() { }
+    /**
+     * Startup a service provider.
+     */
+    async startup() { }
+}
+exports.Extension = Extension;
+/* End of file Extension.ts */ 
